6034ec4363104054e62d46eb658b7d65878d06c1
Achanya John <37082193+Jachanya@users.noreply.github.com>
2022-05-07 14:58:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -92,8 +92,9 @@ function renderPlaces(places) {
         const icon = document.createElement('a-image');
         icon.setAttribute('gps-entity-place', 'latitude: ${latitude}; longitude: ${longitude}');
         icon.setAttribute('name', place.name);
-        icon.setAttribute('src', '../assets/map-marker.png');
+        icon.setAttribute('src', './assets/map-marker.png');
 
+        console.log('here');
         // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
         icon.setAttribute('scale', '20, 20');
 
